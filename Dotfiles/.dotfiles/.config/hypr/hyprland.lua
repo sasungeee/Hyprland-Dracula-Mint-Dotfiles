8d79bf7d4559830d060852a97b98ab9fe0c94e2b
sasungeee <134379671+sasungeee@users.noreply.github.com>
2026-05-22 18:15:35 +0300

    Update hyprland.lua

--- Dotfiles/.dotfiles/.config/hypr/hyprland.lua
+++ Dotfiles/.dotfiles/.config/hypr/hyprland.lua
@@ -222,7 +222,7 @@ hl.bind(mainMod .. " + down",  hl.dsp.focus({ direction = "down" }))
 for i = 1, 10 do
     local key = i % 10
     hl.bind(mainMod .. " + " .. key,             hl.dsp.focus({ workspace = i}))
-    hl.bind(mainMod .. " + SHIFT + " .. key,     hl.dsp.window.move({ workspace = i }))
+    hl.bind(mainMod .. " + SHIFT + " .. key,     hl.dsp.window.move({ workspace = i, follow = false }))
 end
 
 -- Move/resize windows
