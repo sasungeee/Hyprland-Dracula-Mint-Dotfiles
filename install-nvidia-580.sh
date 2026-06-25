629a2c85f1bdb3fb5defc9fb9c9524f0c2850e48
sasungeee <134379671+sasungeee@users.noreply.github.com>
2026-06-25 13:46:25 +0300

    Add script to install NVIDIA 580 drivers and libraries

---
+++ install-nvidia-580.sh
@@ -0,0 +1,3 @@
+yay -S nvidia-580xx-dkms dkms nvidia-580xx-utils opencl-nvidia-580xx cuda-12.9 lib32-nvidia-580xx-utils
+
+sudo pacman -S lib32-amdvlk lib32-vulkan-dzn lib32-vulkan-gfxstream lib32-vulkan-intel lib32-vulkan-swrast amdvlk vulkan-dzn vulkan-gfxstream vulkan-intel vulkan-swrast libgl vulkan-icd-loader vulkan-tools lib32-libgl lib32-vulkan-icd-loader gamemode lib32-gamemode vulkan-intel lib32-vulkan-intel libva libva-nvidia-driver
