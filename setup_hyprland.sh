d7aa7f2533abcec73ef07cce15758fdcee5f2a84
sasungeee <134379671+sasungeee@users.noreply.github.com>
2026-07-03 11:07:28 +0300

    Make PineconeMC.desktop executable

--- setup_hyprland.sh
+++ setup_hyprland.sh
@@ -142,6 +142,7 @@ if [ $? -eq 0 ]; then
     chmod +x ~/.dotfiles/.config/rofi/rofi-power-menu.sh
     chmod +x ~/.dotfiles/.config/hypr/rofi-power-menu-alt-f4.sh
     chmod +x ~/.dotfiles/.config/hypr/show_layout_popup.sh
+    chmod +x ~/.dotfiles/.local/share/applications/PineconeMC.desktop
 
     stow -d ~/.dotfiles/ .
 
