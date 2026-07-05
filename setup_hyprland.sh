3682506c7beff4cf8238b3f003ef2bd07e5020ad
sasungeee <134379671+sasungeee@users.noreply.github.com>
2026-07-05 11:10:43 +0300

    Update bibata cursor theme installation method
    Replaced bibata cursor theme installation with manual download and extraction.

--- setup_hyprland.sh
+++ setup_hyprland.sh
@@ -41,7 +41,11 @@ if [ $? -eq 0 ]; then
     # Instalation of the base software packages
     sudo pacman -Sy --noconfirm wayland xorg xorg-server xorg-xwayland hyprland xdg-desktop-portal xdg-desktop-portal-hyprland xdg-desktop-portal-gtk kitty alacritty neovim firefox pavucontrol pamixer nemo grim slurp satty rofi rofi-calc rofi-emoji waybar mako ttf-jetbrains-mono ttf-jetbrains-mono-nerd ttf-firacode-nerd noto-fonts-emoji otf-font-awesome file-roller hyprpicker hypridle hyprlock btop wlsunset geoclue2 python3 swayimg swaybg nodejs npm eza fish fuse2 wl-clipboard wl-clip-persist mc udiskie vlc mpv unzip flatpak fastfetch zenity gsimplecal flameshot mission-center yad qt5ct qt5 qt5-graphicaleffects qt5-quickcontrols2 qt5-svg qt6ct sddm playerctl xed stow hyprpolkitagent gzip unrar bzip2 xz 7zip ffmpeg dav1d foot swayosd code
 
-    yay -S --noconfirm bibata-cursor-theme-bin
+    mkdir /tmp/bibata/
+    curl -L https://github.com/ful1e5/Bibata_Cursor/releases/latest/download/Bibata-Modern-Classic.tar.xz -o /tmp/bibata/Bibata-Modern-Classic.tar.xz
+    tar -xJvf /tmp/bibata/Bibata-Modern-Classic.tar.xz -C /tmp/bibata/
+    sudo cp -r /tmp/bibata/Bibata-Modern-Classic/ /usr/share/icons/
+    rm -rf /tmp/bibata/
 
     flatpak install flathub com.saivert.pwvucontrol
 
