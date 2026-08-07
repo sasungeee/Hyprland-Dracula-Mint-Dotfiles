cb0e9506b7fccb6d05573c41523d5dcb74055572
sasungeee <134379671+sasungeee@users.noreply.github.com>
2026-08-07 16:42:01 +0300

    Add mesa-utils and lib32-mesa-utils to installation

--- install-notebook-drivers.sh
+++ install-notebook-drivers.sh
@@ -1,3 +1,3 @@
-sudo pacman -S mesa lib32-mesa vulkan-intel lib32-vulkan-intel vulkan-icd-loader lib32-vulkan-icd-loader linux-firmware intel-media-driver libva libva-utils intel-compute-runtime level-zero-loader vulkan-tools gamemode lib32-gamemode ffmpeg gst-libav clinfo intel-oneapi-toolkit sof-firmware pipewire pipewire-pulse pipewire-alsa pipewire-jack wireplumber intel-ucode power-profiles-daemon bluez bluez-utils brightnessctl
+sudo pacman -S mesa lib32-mesa mesa-utils lib32-mesa-utils vulkan-intel lib32-vulkan-intel vulkan-icd-loader lib32-vulkan-icd-loader linux-firmware intel-media-driver libva libva-utils intel-compute-runtime level-zero-loader vulkan-tools gamemode lib32-gamemode ffmpeg gst-libav clinfo intel-oneapi-toolkit sof-firmware pipewire pipewire-pulse pipewire-alsa pipewire-jack wireplumber intel-ucode power-profiles-daemon bluez bluez-utils brightnessctl
 
 sudo systemctl enable power-profiles-daemon.service
